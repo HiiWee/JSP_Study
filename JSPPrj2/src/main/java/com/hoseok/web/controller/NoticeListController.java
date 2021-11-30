6663e78387b160dcff3f51b8c3168068aed5a71a
HiiWee <wpdlzhf159@gmail.com>
2021-12-01 02:10:11 +0900

    목록페이지 MVC model2 로 수정하기: Controller추가

---
+++ JSPPrj2/src/main/java/com/hoseok/web/controller/NoticeListController.java
@@ -0,0 +1,74 @@
+package com.hoseok.web.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hoseok.web.entity.Notice;
+
+// 이 페이지에서의 Model은 목록임 따라서 리스트 객체 이용
+@WebServlet("/notice/list")
+public class NoticeListController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Notice> list = new ArrayList<>();
+		
+		String url = "jdbc:mysql://127.0.0.1:3306/hoseok";
+		String sql = "select * from notice";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, "hoseok", "!dlghtjr4948");
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			
+			while(rs.next()) { 
+				int id = rs.getInt("id");
+				String title = rs.getString("title");
+				String memberId = rs.getString("memberId");
+				String content = rs.getString("content");
+				Date regdate = rs.getDate("regdate");
+				int hit = rs.getInt("hit");
+				String files = rs.getString("files");
+				
+				Notice notice = new Notice(
+						id,
+						title,
+						memberId,
+						content,
+						regdate,
+						hit,
+						files
+						);
+				
+				// 목록을 위해 리스트 객체에 담아놓음
+				list.add(notice);
+			}
+			
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("/notice/list.jsp").forward(request, response);
+	}
+}
