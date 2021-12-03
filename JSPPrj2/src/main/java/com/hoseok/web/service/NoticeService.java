ad116b6238fa177a5dffa50b4f7a47ddc0c48d91
HiiWee <wpdlzhf159@gmail.com>
2021-12-04 03:02:59 +0900

    공개 설정 이용하기: 공개상태표시

--- JSPPrj2/src/main/java/com/hoseok/web/service/NoticeService.java
+++ JSPPrj2/src/main/java/com/hoseok/web/service/NoticeService.java
@@ -96,6 +96,74 @@ public class NoticeService {
 		}
 		return list;
 	}
+	
+	
+	// 공개된 목록만 반환하는 메소드
+	public List<NoticeView> getNoticeViewPubList(String field, String query, int page) {
+	List<NoticeView> list = new ArrayList<>();
+		
+		String url = "jdbc:mysql://127.0.0.1:3306/hoseok";
+		String sql = "select * "
+				+ "from (select ROW_NUMBER() over(order by n.regdate desc) rownum, n.* from NOTICE_VIEW n "
+				+ "where " + field + " like ? and pub=1) n2 "				// pub=1값 꽂아넣음
+				+ "where n2.rownum between ? and ?";
+				// field를 값을 세팅하듯 ?와 st.setString()을 이용해 값을 넣으면 양쪽에 홑따옴표 입력됨 'TITLE'
+				// 하지만 TITLE은 값이 아니므로 문자열 더하기를 이용한다.\
+		
+		//TODO 페이지변수로 볌위 구하기
+			//1, 11, 21, 31 -> an = 1 + (page - 1) * 10
+			//10, 20, 30, 40 -> page * 10
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, "hoseok", "!dlghtjr4948");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, "%" + query + "%");
+			st.setInt(2, 1 + (page - 1) * 10);
+			st.setInt(3, page * 10);
+			
+			
+			ResultSet rs = st.executeQuery();
+			
+			
+			while(rs.next()) { 
+				int id = rs.getInt("id");
+				String title = rs.getString("title");
+				String memberId = rs.getString("memberId");
+				Date regdate = rs.getDate("regdate");
+				int hit = rs.getInt("hit");
+				String files = rs.getString("files");
+				boolean pub = rs.getBoolean("pub");
+				int cmtCount = rs.getInt("cmt_count");
+				
+				NoticeView notice = new NoticeView(
+						id,
+						title,
+						memberId,
+						regdate,
+						hit,
+						files,
+						pub,
+						// content, --> 용량문제로 뷰에서 삭제
+						cmtCount
+						);
+				
+				// 목록을 위해 리스트 객체에 담아놓음
+				list.add(notice);
+			}
+			
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
 
 	
 	
@@ -402,5 +470,7 @@ public class NoticeService {
 	List<Notice> getNoticeNewestList() {
 		return null;
 	}
+
+
 	
 }
