2becf299905cd4e57e8e28066cf0b083a310c4d0
HiiWee <wpdlzhf159@gmail.com>
2021-12-07 05:14:40 +0900

    영화 예매 서버 : index controller

---
+++ MovieReservation/src/main/java/com/movie/web/controller/member/IndexController.java
@@ -0,0 +1,19 @@
+package com.movie.web.controller.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/member/index")
+public class IndexController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+}
