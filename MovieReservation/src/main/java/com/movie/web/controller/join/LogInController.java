7cc54023100b638952ec0787838b481879d1c5f9
HiiWee <wpdlzhf159@gmail.com>
2021-12-07 05:19:12 +0900

    영화 예매 서버 : 로그인 controller 구현

---
+++ MovieReservation/src/main/java/com/movie/web/controller/join/LogInController.java
@@ -0,0 +1,59 @@
+package com.movie.web.controller.join;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.movie.web.entity.Member;
+import com.movie.web.service.MemberService;
+
+@WebServlet("/join/login")
+public class LogInController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		request.getRequestDispatcher("login.jsp").forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memberId_ = request.getParameter("memberId");
+		String pwd_ = request.getParameter("pwd");
+		
+		String memberId = "";
+		String pwd = "";
+		
+		if (memberId_ != null && !memberId_.equals(""))
+			memberId = memberId_;
+		
+		if (pwd_ != null && !pwd_.equals(""))
+			pwd = pwd_;
+	
+		
+		MemberService service = new MemberService();
+		int result = service.logInMember(memberId, pwd);
+		
+		if (result == 1) {
+			System.out.println("로그인 성공");
+			HttpSession session = request.getSession();
+			session.setAttribute("sessionId", memberId);
+			response.sendRedirect("/member/index");
+		}
+		else if (result == -1) {
+			System.out.println("존재하지 않는 아이디 입니다.");
+			request.setAttribute("idChk", result);
+			response.sendRedirect("login");
+		}
+		else if (result == 0) {
+			System.out.println("비밀번호가 틀렸습니다.");
+			request.setAttribute("idChk", result);
+			response.sendRedirect("login");
+		}
+	}
+}
