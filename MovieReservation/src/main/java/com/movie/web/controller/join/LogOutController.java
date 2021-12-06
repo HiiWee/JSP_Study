539fcdf875836545c7484ce10247a6388f8b3b13
HiiWee <wpdlzhf159@gmail.com>
2021-12-07 05:19:40 +0900

    영화 예매 서버 : 로그아웃 controller구현

---
+++ MovieReservation/src/main/java/com/movie/web/controller/join/LogOutController.java
@@ -0,0 +1,30 @@
+package com.movie.web.controller.join;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/join/logout")
+public class LogOutController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		System.out.println("세션 삭제");
+		response.sendRedirect("/index");
+	}
+}
