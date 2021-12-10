e7b27532ec90da1c24b89d4c9a374ab10fbe1cab
HiiWee <wpdlzhf159@gmail.com>
2021-12-10 13:40:25 +0900

    회원 정보 수정위한 서비스 추가

--- MovieReservation/src/main/java/com/movie/web/service/MemberService.java
+++ MovieReservation/src/main/java/com/movie/web/service/MemberService.java
@@ -85,6 +85,79 @@ public class MemberService {
 		return -1;	// 없는 아이디
 	}
 
+
+	public Member getMember(String id) {
+		
+		String url = "jdbc:mysql://127.0.0.1:3306/theater_db";
+		String sql = "select * from member where memberId = ?";
+		
+		Member member = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, "hoseok", "!dlghtjr4948");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, id);
+			ResultSet rs = st.executeQuery();
+			
+			rs.next();
+			String memberId = rs.getString("memberId");
+			String name = rs.getString("name");
+			String pwd = rs.getString("pwd");
+			String phone = rs.getString("phone");
+			
+			member = new Member(memberId, name, pwd, phone, false);
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return member;
+	}
+
+
+	public int updateMember(Member member) {
+		int result = 0;
+		
+		String url = "jdbc:mysql://127.0.0.1:3306/theater_db";
+		String sql = "update member set name=?, pwd=?, phone=?  where memberId = ?";
+		
+		// 가입시 비밀번호가 다르면 -1 반환
+		if (member.getPwd().equals("")) {
+			return -1;
+		}
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, "hoseok", "!dlghtjr4948");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, member.getName());
+			st.setString(2, member.getPwd());
+			st.setString(3, member.getPhone());
+			st.setString(4, member.getMemberId());
+			result = st.executeUpdate();
+
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+
+
 }
 
 // 회원가입을 위한 서비스
